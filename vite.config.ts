d134f1893958fc74dfaccf246910c603c84216e1
Nicholas Wise <nicholasaragih@gmail.com>
2026-06-24 16:50:09 +0700

    init styling dependencies

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,15 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+    plugins: [react()],
+    compilerOptions: {
+        // ... all the existing Vite options ...
+
+        baseUrl: ".",
+        paths: {
+            "@/*": ["./src/*"],
+        },
+    },
+});
