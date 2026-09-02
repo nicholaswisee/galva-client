c60258a70180dada6cf4c53cd6f09a6c30c960f9
Nicholas Wise <nicholasaragih@gmail.com>
2026-09-02 14:20:54 +0700

    Refresh development service worker

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -85,7 +85,7 @@ define(['./workbox-f0c192c2'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "index.html",
-    "revision": "0.didgjo9ui7c"
+    "revision": "0.8fpmrifka8c"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
